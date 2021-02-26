83a7c89ad24cdfac1f088afc3b9cd2598b51a805
Jeevan Bastola <jeevanbastola986@gmail.com>
2021-02-25 22:10:57 -0500

    fingers crossed

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Jeevan  Bastola on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIView!
+    
+    
+}
